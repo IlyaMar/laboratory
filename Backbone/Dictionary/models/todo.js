16c9d456f2aeb618b9b452dae1b9aab03ced3238
Ilya <imartynovsp@gmail.com>
2014-06-29 14:58:26 +0400

    added backbone folder

---
+++ Backbone/Dictionary/models/todo.js
@@ -0,0 +1,26 @@
+ var app = app || {};
+
+(function () {
+	'use strict';
+
+ // Todo Model
+  // ----------
+  // Our basic **Todo** model has 'title', 'order', and 'completed' attributes.
+  app.Todo = Backbone.Model.extend({
+    // Default attributes ensure that each todo created has `title` and 
+    // `completed` keys.
+    defaults: {
+      title: '',
+      completed: false
+    },
+    // Toggle the `completed` state of this todo item.
+    toggle: function() {
+  	  console.log('Todo toggle');
+
+	  this.save({
+        completed: !this.get('completed')
+      });
+    }
+  });
+  
+})();
